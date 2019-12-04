ffe5a3ac0bfb8bbe5c7a4122b4026aa135903a46
marcusgardh <58325557+marcusgardh@users.noreply.github.com>
2019-12-04 09:38:21 +0100

    produkter visas upp på skärmen

--- Gruppuppgift/js/main.js
+++ Gruppuppgift/js/main.js
@@ -19,7 +19,7 @@ function createProducts() {
     let product1 = new Product();
     product1.id = 1;
     product1.title = "Soothe Your Soul";
-    product1.image = "bild.jpg";
+    product1.image = "images/product_1.jpg";
     product1.price = 1;
     product1.description = "Vacker tavla med ett abstrakt motiv i stilfulla färgtoner av vitt, grått, svart och guld. Illustrationen är gjord av Elisabeth Fredriksson som är känd för sina färgrika och eleganta illustrationer.";
     products.push(product1);
@@ -27,7 +27,7 @@ function createProducts() {
     let product2 = new Product();
     product2.id = 2;
     product2.title = "Deep Teal Stone";
-    product2.image = "bild.jpg";
+    product2.image = "images/product_2.jpeg";
     product2.price = 1;
     product2.description = "Grafiskt och elegant print med motiv av ett grönblått mönster och eleganta guldiga linjer. Postern passar lika bra för sig själv som i kombination med andra posters i en trendig tavelvägg.";
     products.push(product2);
@@ -35,7 +35,7 @@ function createProducts() {
     let product3 = new Product();
     product3.id = 3;
     product3.title = "Corridor";
-    product3.image = "bild.jpg";
+    product3.image = "images/product_3.jpg";
     product3.price = 1;
     product3.description = "Stilren svartvit poster av en lång korridor i vacker arkitektur där solljusets strålar skapar skuggmönster i marken.";
     products.push(product3);
@@ -43,7 +43,7 @@ function createProducts() {
     let product4 = new Product();
     product4.id = 4;
     product4.title = "Elephant";
-    product4.image = "bild.jpg";
+    product4.image = "images/product_4.jpg";
     product4.price = 1;
     product4.description = "En fotokonst poster med motiv av en vacker och ståtlig elefant. Bilden går i svartvita toner med otroliga detaljer, vilket ger en känsla av att du nästan står där själv och möter det storslagna djuret.";
     products.push(product4);
@@ -51,7 +51,7 @@ function createProducts() {
     let product5 = new Product();
     product5.id = 5;
     product5.title = "Boat in the Lake";
-    product5.image = "bild.jpg";
+    product5.image = "images/product_5.jpeg";
     product5.price = 1;
     product5.description = "En vacker naturposter med motiv av en vattenfylld dal som omringas av kantiga berg. Närmst synfältet syns en trätrappa som leder ner i vattnet och bort i horisonten. Tätt intill trätrappan ligger en träbåt vilket ger ett intressant liv till den kraftfulla naturbilden.";
     products.push(product5);
@@ -59,7 +59,7 @@ function createProducts() {
     let product6 = new Product();
     product6.id = 6;
     product6.title = "Blossom";
-    product6.image = "bild.jpg";
+    product6.image = "images/product_6.jpg";
     product6.price = 1;
     product6.description = "Vacker natur poster med bild på en blommande ros. Blossom passar perfekt att kombinera med andra botaniska motiv och den gör sig väldigt bra i en tavelvägg.";
     products.push(product6);
@@ -67,7 +67,7 @@ function createProducts() {
     let product7 = new Product();
     product7.id = 7;
     product7.title = "Waterfall";
-    product7.image = "bild.jpg";
+    product7.image = "images/product_7.jpg";
     product7.price = 1;
     product7.description = "Stilren naturposter med ett hisnande vackert fotografi av ett vattenfall. Tavlans mörka gröna toner gör att den ger en lugnande effekt och är enkel att kombinera med andra naturmotiv och citat.";
     products.push(product7);
@@ -75,15 +75,38 @@ function createProducts() {
     let product8 = new Product();
     product8.id = 8;
     product8.title = "Pink Wall";
-    product8.image = "bild.jpg";
+    product8.image = "images/product_8.jpg";
     product8.price = 1;
     product8.description = "Vacker poster med ett fotografiskt motiv av en rosa vägg med ett vackert hålmönster i marockansk stil. Det ihåliga mönstret skapar en vacker och hänförande dynamik.";
     products.push(product8);
 }
 
 function displayProducts() {
+
+    let container = document.getElementById("productcontainer");
+
     for (let i = 0; i < products.length; i++) {
         console.log(products[i].title + " - " + products[i].id);
         console.log(products[i].description);
+
+        let product = document.createElement("div");
+        product.className = "col-6 col-md-3 productdiv";
+        container.appendChild(product);
+
+        let imageDiv = document.createElement("div");
+        // imageDiv.className = "imagediv";
+        let image = document.createElement("img");
+        image.src = products[i].image;
+        image.className = "img-fluid"
+        product.appendChild(imageDiv);
+        imageDiv.appendChild(image);
+
+        let title = document.createElement("p");
+        title.innerHTML = products[i].title;
+        product.appendChild(title);
+
+        let price = document.createElement("p");
+        price.innerHTML = products[i].price + " kr";
+        product.appendChild(price);
     }
 }

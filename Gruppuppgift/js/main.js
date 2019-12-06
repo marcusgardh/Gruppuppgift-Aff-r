7b3866bf5da9e866baae8ca6f854a8fdfdca8395
Julia-Nilsson <jniilsson@live.se>
2019-12-06 09:46:50 +0100

    ö

--- Gruppuppgift/js/main.js
+++ Gruppuppgift/js/main.js
@@ -114,7 +114,7 @@ function displayProducts() {
     }
 
     $( "#toCheckOut" ).click(function() {
-        window.location.href = "html/checkout.html";
+        window.location = "html/checkout.html";
       });
     
 }
